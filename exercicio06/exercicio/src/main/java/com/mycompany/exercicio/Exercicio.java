214e4455f554a96789a14f6856958fc0f37974ac
Julia Silva <111365788+juliacpa73@users.noreply.github.com>
2024-02-15 23:11:35 -0300

    Add files via upload

---
+++ exercicio06/exercicio/src/main/java/com/mycompany/exercicio/Exercicio.java
@@ -0,0 +1,17 @@
+package com.mycompany.exercicio;
+
+import java.util.Scanner;
+
+public class Exercicio {
+
+    public static void main(String[] args) {
+        /* Scanner ju = new Scanner(System.in);
+        System.out.println("Exercício 1");
+        int x1 = ju.nextInt();
+        int x2 = ju.nextInt();
+        int soma = x1 + x2;
+        System.out.println("A soma é " + soma);
+        ju.close();
+         */
+    }
+}
